56091988fd24eb95417151ccdbdea793c51cae61
lidianvilazio <lidianvilazio@hotmail.com>
2018-03-21 18:31:41 -0400

    Done.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Category, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe 'Category' do
+
+  before do
+    @category = Category.create(name: "boooooo")
+  end
+
+  it 'has a name' do
+    expect(@category.name).to eq("boooooo")
+  end
 end
--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe PostCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe PostCategory, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe 'post_categories' do
+
+  before do
+    @category = Category.create(name: "Cool")
+    @post = Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
+    @post_categorie = PostCategory.create(post: @post, category: @category)
+  end
+  it 'belongs_to post' do
+    expect(@post_categorie.post).to be(@post)
+  end
+
+  it 'belongs_to category' do
+    expect(@post_categorie.category).to be(@category)
+  end
 end
